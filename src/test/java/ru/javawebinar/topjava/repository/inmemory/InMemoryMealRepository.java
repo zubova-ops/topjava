72ddeb34f5a0b6e19017cd0ad549b0a3cc5cf86c
Staymix <staszubov199530@gmail.com>
2025-12-02 15:45:46 +0500

    Apply 4_7_create_inmemory_test_ctx.patch

--- src/test/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/test/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -34,7 +34,6 @@ public class InMemoryMealRepository implements MealRepository {
     private final AtomicInteger counter = new AtomicInteger(0);
 
     {
-        MealsUtil.meals.forEach(meal -> save(meal, USER_ID));
         save(new Meal(LocalDateTime.of(2015, Month.JUNE, 1, 14, 0), "Админ ланч", 510), ADMIN_ID);
         save(new Meal(LocalDateTime.of(2015, Month.JUNE, 1, 21, 0), "Админ ужин", 1500), ADMIN_ID);
     }
